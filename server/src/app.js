ff62a8a44b570147d4fbcc534b35e456710a5d82
Anudeep Chandra Paul <anudeepch.paul@gmail.com>
2018-05-26 09:03:44 +0530

    rest api updates

--- server/src/app.js
+++ server/src/app.js
@@ -28,17 +28,87 @@ require('./routes')(app)
 //   if (err) throw err
 
 //   const db = client.db(config.db.name)
-//   mongo.workerDb = db
+//   mongo.workerDb = d
+
+class Store {
+  constructor() {
+    this.data = []
+  }
+  
+  get(id) {
+      return id ? this.data.filter(el => (el.id === id)): this.data.concat();
+  }
+  
+  set(data) {
+    this.data.push(data);
+  }
+  
+  update(id, data) {
+    let index = this.data.findIndex(el => (el.id === id));
+    
+    if(index !== null || index !== undefined) {
+      this.data[index] = data;
+    }
+  }
+  
+  delete(id) {
+    let index = this.data.findIndex(el => (el.id === id));
+    
+    if(index !== null || index !== undefined) {
+      this.data.splice(index, 1);
+    }
+  }
+}
+
+var storage = new Store();
 
 app.get('/data', function(req, res) {
+  try {
+      res.send({
+      success: true,
+      result: {
+          data: storage.get(req.params.id)
+        }
+      }
+   } catch(e) {
+      res.send({
+      error: true,
+      result: {
+          data: null,
+          msg: 'error occurred'
+      }
+   }
   
 });
 app.post('/data', function(req, res) {
-    
+    try {
+      let data = storage.get(req.params.id) || null;
+      
+      if(!data) {
+          // read the form body req.body
+          // set the data to the array
+      }
+      
+      res.send({
+        success: true,
+        result: {
+            data: data
+          }
+      }
+   } catch(e) {
+      res.send({
+        error: true,
+        result: {
+            data: null,
+            msg: 'error occurred'
+        }
+      });
+  }
 });
 
 app.listen(config.port,
   console.log(`Server started at ${config.port}.`)
+           
 })
 
 // sequelize.sync({ force: false }).then(() => {
